a5eb655a8959c3e17c7a3f9d9e6c29b6fa45c0d6
Ashwini Iyer <Ashwini.Iyer@bestbuy.com>
2021-11-12 19:51:29 -0800

    added shell script file

---
+++ src/test/resources/brokenlinks.sh
@@ -0,0 +1,9 @@
+#This is to find the broken links in webpage
+echo -n "enter the url to test"
+read url 
+Echo "Your URL Entered : $url"
+java -jar  /Users/A6004580/IdeaProjects/adweb/target/brokenLinks.jar $url
+
+
+# please put your jar file path here
+
